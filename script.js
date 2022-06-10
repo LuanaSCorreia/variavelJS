5cf502400083c660c46e0868b46d4e700ffe4b8d
LuanaSCorreia <luanacorreia797@gmail.com>
2022-06-09 21:43:56 -0300

    estrutura de dados

---
+++ script.js
@@ -0,0 +1,17 @@
+alert("Alguem aí?")
+
+let nome ="luana";
+
+let number = 22;
+
+let ano = 2022;
+
+let sub = ano - number;
+
+let soma = ano + number;
+console.log(soma);
+
+console.log("Estou mexendo no console.log");
+
+let quartaFeira = true;
+console.log(typeof(quartaFeira));
